af79e419d685feb94baafd3afbbe8cd8a34590cb
rlawlsdnjs <orochi13@naver.com>
2016-05-02 15:10:31 +0900

    myapp

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds013162.mlab.com:13162/babamba");
+var db = mongoose.connection;
+db.once("open", function(){
+  console.log("DB connected!");
+});
+db.on("error", function(err){
+  console.log("DB ERROR :", err);
+});
 
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public' )));
