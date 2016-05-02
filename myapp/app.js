564fbf7bfad5c2ae612b04f112b9dd263f576db2
rlawlsdnjs <orochi13@naver.com>
2016-05-02 17:07:45 +0900

    myapp

--- myapp/app.js
+++ myapp/app.js
@@ -17,8 +17,8 @@ db.on("error", function(err){
 var postSchema = mongoose.Schema({
   title: {type:String, required:true},
   body: {type:String, required:true},
-  createdAT: {type:Data, default:Date.now},
-  updatedAT: date
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
 });
 var Post = mongoose.model('Post', postSchema);
 
@@ -34,15 +34,35 @@ app.use(bodyParser.json());
 app.get('/posts', function(req, res){
   Post.find({} , function(err,posts){
     if(err) return res.json({success:false, message:err});
-    res.json({sucess:true, data:posts});
+    res.json({success:true, data:posts});
   });
 }); //index
 app.post('/posts', function(req, res){
   Post.create(req.body.post, function(err,post){
-    if(err) return res.json({succes:false, message:err});
+    if(err) return res.json({success:false, message:err});
     res.json({success:true, data:post});
   });
 }); // create
+app.get('/posts/:id', function(req, res){
+  Post.findById(req,params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
+  });
+});
+app.put('posts/:id', function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+"updated"});
+  });
+}); //update
+app.delete('/posts/:id', function(req,res){
+  Post.findByIdAndRemove(req,params.id, function(err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+"deleted"});
+  });
+}); //destroy
+
 //start server
 
 
