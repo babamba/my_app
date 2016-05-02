22f60003396d0cf427586a93a56a3c3811723e9d
rlawlsdnjs <orochi13@naver.com>
2016-05-02 16:32:56 +0900

    myapp

--- myapp/app.js
+++ myapp/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
@@ -12,49 +13,38 @@ db.on("error", function(err){
   console.log("DB ERROR :", err);
 });
 
-var dataSchema = mongoose.Schema({
-  name : String,
-  count: Number
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAT: {type:Data, default:Date.now},
+  updatedAT: date
 });
+var Post = mongoose.model('Post', postSchema);
 
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"}, function(err,data){
-  if(err) return console.log("Data ERROR:",err);
-  if(!data){
-    Data.create({name:"myData", count:0}, function(err,data){
-      if(err) return console.log("Data ERROR:", err);
-      console.log("Counter initialized :", data);
-    });
-  }
-});
+// view setting
 
 app.set("view engine", 'ejs');
+
+// set middlewares
 app.use(express.static(path.join(__dirname, 'public' )));
+app.use(bodyParser.json());
 
-
-app.get('/', function(req,res){
-  Data.findOne({name:"myData"}, function(err,data){
-    if(err) return console.log("Data ERROR:", err);
-    data.count++;
-    data.save(function (err){
-      if(err)return console.log("Data ERROR:",err);
-      res.render("my_first_ejs",data);
-    });
+// set routes
+app.get('/posts', function(req, res){
+  Post.find({} , function(err,posts){
+    if(err) return res.json({success:false, message:err});
+    res.json({sucess:true, data:posts});
   });
-});
+}); //index
+app.post('/posts', function(req, res){
+  Post.create(req.body.post, function(err,post){
+    if(err) return res.json({succes:false, message:err});
+    res.json({success:true, data:post});
+  });
+}); // create
+//start server
 
-app.get('/reset', function (req,res){
-  data.count=0;
-  res.render('my_first_ejs',data);
-});
-app.get('/set/count', function (req,res){
-  if(req.query.count) data.count=req.query.count;
-  res.render('my_first_ejs',data);
-});
-app.get('/sum/:num', function (req,res){
-  data.count=req.params.num;
-  res.render('my_first_ejs',data);
-});
 
 app.listen(3003, function(){
   console.log('3003 port server on!');
