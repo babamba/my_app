d39337f953b910e7333f36d447739fd5205f148b
rlawlsdnjs <orochi13@naver.com>
2016-05-02 13:32:24 +0900

    myapp

--- myapp/app.js
+++ myapp/app.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
 
-app.use(express.static(path.join(__dirname + '/public' )));
-app.listen(3000, function(){
-  console.log('3000 port server on!');
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, 'public' )));
+
+var data={count:0};
+app.get('/', function (req, res){
+    data.count++;
+    res.render('my_first_ejs',data);
+});
+app.get('/reset', function (req,res){
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+app.get('/set/count', function (req,res){
+  if(req.query.count) data.count=req.query.count;
+  res.render('my_first_ejs',data);
+});
+app.get('/sum/:num', function (req,res){
+  data.count=req.params.num;
+  res.render('my_first_ejs',data);
+});
+app.listen(3003, function(){
+  console.log('3003 port server on!');
 });
